1be50880f11bf90fce68ecc3f6869eecf7f202b0
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-08-20 03:39:57 +0800

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,53 @@
 import Plane from "../assets/images/plane.png"
 
+//<a href=""> href need to be updated
 export default function Footer() {
   return (
     <footer>
-      <div className="footer--container--title">
+      <div className="footer--title">
         <h2>Explore the world with My Dream place</h2>
-        <a>Discover new places and experiences</a>
+        <a href="">Discover new places and experiences</a>
       </div>
-      <div className="footer--container--ul">
+      <div className="footer--links">
         <ul>
           <li>
             <img src={Plane} alt="plane icon" className="plane"/>
-            <span>My Dream Place</span>
+            <span>my Dream Place</span>
           </li>
-          <li>Your next goto companion for travel</li>
+          <li id="next-goto">Your next goto companion for travel</li>
         </ul>
         <ul>
           <li>Company</li>
-          <li><a>About</a></li>
-          <li><a>Jobs</a></li>
-          <li><a>Newsroom</a></li>
-          <li><a>Advertising</a></li>
-          <li><a>Contact us</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Jobs</a></li>
+          <li><a href="">Newsroom</a></li>
+          <li><a href="">Advertising</a></li>
+          <li><a href="">Contact us</a></li>
         </ul>
         <ul>
           <li>Explore</li>
-          <li><a>Australia</a></li>
-          <li><a>New Zealand</a></li>
-          <li><a>United States America (USA)</a></li>
-          <li><a>Greece</a></li>
-          <li><a>Maldives</a></li>
-          <li><a>Singapore</a></li>
-          <li><a>See more</a></li>
+          <li><a href="">Australia</a></li>
+          <li><a href="">New Zealand</a></li>
+          <li><a href="">United States America (USA)</a></li>
+          <li><a href="">Greece</a></li>
+          <li><a href="">Maldives</a></li>
+          <li><a href="">Singapore</a></li>
+          <li><a href="" id="see-more">See more</a></li>
         </ul>
         <ul>
           <li>Terms and Policies</li>
-          <li><a>Privacy Policy</a></li>
-          <li><a>Terms of use</a></li>
-          <li><a>Accessibility</a></li>
-          <li><a>Reward system policy</a></li>
+          <li><a href="">Privacy Policy</a></li>
+          <li><a href="">Terms of use</a></li>
+          <li><a href="">Accessibility</a></li>
+          <li><a href="">Reward system policy</a></li>
         </ul>
         <ul>
-          <li><a>Help</a></li>
-          <li><a>Support</a></li>
-          <li><a>Cancel your bookings</a></li>
-          <li><a>Use Coupon</a></li>
-          <li><a>Refund Policies</a></li>
-          <li><a>International Travel Documents</a></li>
+          <li>Help</li>
+          <li><a href="">Support</a></li>
+          <li><a href="">Cancel your bookings</a></li>
+          <li><a href="">Use Coupon</a></li>
+          <li><a href="">Refund Policies</a></li>
+          <li><a href="">International Travel Documents</a></li>
         </ul>
       </div>
       <p>Copyright &copy; bookly.com. Make with ❤️.</p>
